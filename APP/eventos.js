50ee3bd787ce9ad4eb1a2c942ca83eafde426e80
Plams22v <juanca.tabares34@gmail.com>
2025-04-28 21:54:42 -0500

    Add files via upload
    v1.0

--- APP/eventos.js
+++ APP/eventos.js
@@ -1,1 +1,17 @@
 //hola
+const boton = document.getElementById('btnsubir');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 10) { // cuando baja 300px aparece
+      boton.style.display = 'block';
+    } else {
+      boton.style.display = 'none';
+    }
+  });
+
+  function subirA() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
