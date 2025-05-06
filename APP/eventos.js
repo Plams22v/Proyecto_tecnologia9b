c65942beba57776d61387e2b3a063fb6eeae9a3d
Plams22v <juanca.tabares34@gmail.com>
2025-05-05 21:28:05 -0500

    Add files via upload

--- APP/eventos.js
+++ APP/eventos.js
@@ -1,4 +1,4 @@
-//hola
+//hola :v
 const boton = document.getElementById('btnsubir');
 
   window.addEventListener('scroll', function() {
@@ -15,3 +15,33 @@ const boton = document.getElementById('btnsubir');
       behavior: 'smooth'
     });
   }
+
+  let index = 0;
+  const slides = document.querySelectorAll('.slide');
+  const slidesContainer = document.querySelector('.slides');
+  
+  function mostrarSlide(i) {
+    index = (i + slides.length) % slides.length;
+    const slideWidth = slides[0].clientWidth;
+    slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+  
+  
+  function siguiente() {
+    mostrarSlide(index + 1);
+  }
+  
+  function anterior() {
+    mostrarSlide(index - 1);
+  }
+  
+  // Automático cada 5s
+  setInterval(() => {
+    siguiente();
+  }, 5000);
+
+  window.addEventListener('resize', () => {
+    mostrarSlide(index);
+  });
+  
+  
